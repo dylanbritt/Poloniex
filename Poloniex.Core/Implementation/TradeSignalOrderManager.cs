c924cf23c63204a520c0e1b5e7c3a048ebc2a8d3
dylan <dylan@DESKTOP-3U0HJ87>
2017-03-28 21:14:59 -0500

    ProcessTradeSignalOrders bug fix

--- Poloniex.Core/Implementation/TradeSignalOrderManager.cs
+++ Poloniex.Core/Implementation/TradeSignalOrderManager.cs
@@ -11,32 +11,37 @@ namespace Poloniex.Core.Implementation
     {
         public static void ProcessTradeSignalOrders(Guid eventActionId)
         {
-            Logger.Write($"Executing ProcessTradeSignalOrders for eventActionId: {eventActionId}", Logger.LogType.TransactionLog);
             using (var db = new PoloniexContext())
             {
                 // get oldest uncompleted order
                 var oldest = db.TradeSignalOrders
                     .Where(x => !x.IsProcessed)
                     .OrderBy(x => x.OrderRequestedDateTime)
-                    .First();
-                oldest.IsProcessed = true;
-                oldest.InProgress = true;
-                db.Entry(oldest).State = EntityState.Modified;
-                db.SaveChanges();
+                    .FirstOrDefault();
 
-                if (oldest.TradeSignalOrderType == TradeSignalOrderType.Buy)
+                if (oldest != null)
                 {
-                    TradeManager.BuyBtcFromUsdt(ref oldest);
-                }
-                else
-                {
-                    TradeManager.SellBtcToUsdt(ref oldest);
-                }
+                    Logger.Write($"Processing TradeSignalOrderId: {oldest.TradeSignalOrderId} for eventActionId: {eventActionId}", Logger.LogType.TransactionLog);
 
-                oldest.OrderCompletedDateTime = DateTime.UtcNow;
-                oldest.InProgress = false;
-                db.Entry(oldest).State = EntityState.Modified;
-                db.SaveChanges();
+                    oldest.IsProcessed = true;
+                    oldest.InProgress = true;
+                    db.Entry(oldest).State = EntityState.Modified;
+                    db.SaveChanges();
+
+                    if (oldest.TradeSignalOrderType == TradeSignalOrderType.Buy)
+                    {
+                        TradeManager.BuyBtcFromUsdt(ref oldest);
+                    }
+                    else
+                    {
+                        TradeManager.SellBtcToUsdt(ref oldest);
+                    }
+
+                    oldest.OrderCompletedDateTime = DateTime.UtcNow;
+                    oldest.InProgress = false;
+                    db.Entry(oldest).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
             };
         }
     }
